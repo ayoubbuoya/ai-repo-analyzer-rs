90bf7c3d847761ae79d1efdabc1dd76b102ba07c
Ayoub Amer <ayouub.ameur@gmail.com>
2025-08-28 21:01:21 +0100

    feat: add qdrant connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use qdrant_client::Qdrant;
 
 mod ingest;
 
@@ -6,7 +7,21 @@ pub const TEST_REPO_URL: &str = "https://github.com/ayoubbuoya/orchestra-rs.git"
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    dotenvy::dotenv().ok();
+
+    // Read the qdrant configuration from environment variables
+    let qdrant_url = std::env::var("QDRANT_URL")?;
+    let qdrant_api_key = std::env::var("QDRANT_API_KEY")?;
+
+    let qdrant_client = Qdrant::from_url(&qdrant_url)
+        .api_key(qdrant_api_key)
+        .build()?;
+
+    println!("Qdrant client built successfully");
+
+    let collections_list = qdrant_client.list_collections().await?;
+
+    dbg!(collections_list);
 
     ingest::fetch_repo(TEST_REPO_URL).await?;
 
